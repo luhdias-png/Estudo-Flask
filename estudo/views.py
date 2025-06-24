8bfc8e8fcdc8d9cabf333fc7bda23328e1fd145c
luhdias-png <andre_lucas.94@hotmail.com>
2025-06-24 17:18:46 -0300

    atualizacao

--- estudo/views.py
+++ estudo/views.py
@@ -1,6 +1,18 @@
 from estudo import app
-from flask import render_template
 
+from flask import render_template, url_for
+
+# A função url_for tem como objetivo gerar URLs dinamicamente com base no nome da função da rota define uma rota para a URL principal ('/')
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    usuario = "mandary master ronaldo"
+    idade = 31
+
+    context = {
+        'usuario': usuario,'idade':idade,
+    }
+    return render_template('index.html', context=context)
+
+@app.route('/contatos')
+def cadastro():
+    return 'É a ambulancia besta!'
